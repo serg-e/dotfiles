f48cfddb5c8beecae33def4a8e71dda450f5d9e6
serg-e <smosesov@gmail.com>
2020-08-12 02:58:34 +0100

    zlib

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 
-sudo apt-get install -y --force-yes \
+sudo apt-get install -y --force-yes --no-install-recommends \
     make \
     build-essential \
     zlib \
